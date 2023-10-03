03671f5558515d421af8fd92430ec5d303e28612
Arthur Mendes <thumendess@gmail.com>
2023-10-03 12:12:40 -0300

    Formalidades

--- app/data.tsx
+++ app/data.tsx
@@ -12,18 +12,12 @@ export const projects: Project[] = [
     name: "Sintesc",
     description: (
       <div className="space-y-3">
-        <p>
-          Foi o sistema em que comecei a carreira de Desenvolvedor Junior. Meu principal trabalho foi a criação e
-          manutenção de formulários complexos contendo integrações com diferentes APIs, sendo a mais desafiadora a API
-          do Google Maps que foi usada para calculo de tempo decorrido entre paradas e também a distância entre os
-          pontos de uma viagem.
-        </p>
-        <p>
-          Além disso, fui responsável pela manutenção e criação de automações. Com base nos formulários anteriores,
-          criei um RPA que acessa plataformas de órgãos públicos e geram documentos que são usados pelos motoristas
-          participantes da cooperativa.
-        </p>
-        <p>Aprendi muito com o projeto e foi com ele que eu consegui alavancar no Desenvolvimento Web.</p>
+        Sistema pelo qual comecei a carreira de Desenvolvedor Junior. Minhas principais responsabilidades envolviam a
+        criação e manutenção de formulários avançados com integrações a diversas APIs, destacando-se a integração com a
+        API do Google Maps, utilizada para calcular tempo e distância entre pontos de uma rota. Adicionalmente,
+        desenvolvi e mantive automações, incluindo um RPA que interage com plataformas de órgãos públicos para gerar
+        documentos essenciais aos motoristas da cooperativa. Este projeto foi fundamental para meu crescimento no
+        Desenvolvimento Web.
       </div>
     ),
     stack: ["Laravel", "Nodejs", "Selenium", "Puppeteer"],
@@ -32,9 +26,10 @@ export const projects: Project[] = [
     name: "Claro",
     description: (
       <p>
-        Minha participação nos projetos da claro foram manutenção e criação de automações em lojas online. O papel dos
-        robôs é scanear preço nas demais lojas concorrentes e outros marketplaces, salvar no banco e com a finalidade do
-        cliente conseguir ter uma base de valores e conseguir tomar decisões em cima das analises.
+        Participei ativamente dos projetos da Claro, focando na manutenção e desenvolvimento de automações para lojas
+        online. Essas automações têm como função escanear e comparar preços de lojas concorrentes e outros marketplaces,
+        armazenando esses dados em um banco. Isso proporciona ao cliente uma base sólida de valores, permitindo tomadas
+        de decisões baseadas em análises concretas.
       </p>
     ),
     stack: ["Laravel", "Nodejs", "Playwright", "Puppeteer"],
@@ -43,13 +38,13 @@ export const projects: Project[] = [
     name: "SST Pro",
     description: (
       <p>
-        O SST é uma comunidade de investidores focado no Day Trade. Este projeto, foi o primeiro que construi a maior
-        parte desde o 0, e foi um divisor de águas na minha experiência. O projeto era uma “simples” aplicação para
-        reunir a comunidade do SST em um lugar, inspirado no discord, contem um chat tempo-real podendo também fazer
-        chamadas de voz (integrando uma API agora.io para as chamadas), além de um conjunto de ferramentas focadas no
-        investimento que são liberadas através de uma integração com a Hotmart. Meu papel foi a construção de quase todo
-        o projeto, incluindo os CRUDS, as integrações com agora.io e hotmart, além de ser o projeto em que usei
-        microserviços pela primeira vez.
+        O SST é uma plataforma dedicada à comunidade de investidores com ênfase em Day Trade. Fui fundamental na
+        construção deste projeto desde sua concepção, o que marcou um momento decisivo em minha trajetória profissional.
+        Inspirado no Discord, o sistema possui um chat em tempo real e capacidade de chamadas de voz, integrando a API
+        da agora.io. Adicionalmente, oferece ferramentas de investimento disponibilizadas por meio da integração com a
+        Hotmart. Fui responsável por desenvolver a maior parte das funcionalidades, incluindo os CRUDs e integrações com
+        as APIs mencionadas. Importante destacar que a implementação de microserviços, que utilizei pela primeira vez
+        neste projeto, foi uma ideia que propus.
       </p>
     ),
     stack: ["Nodejs", "Express", "React", "Typescript", "Socket.io", "Agora.io", "Mongodb"],
@@ -58,10 +53,11 @@ export const projects: Project[] = [
     name: "GPTexto",
     description: (
       <p>
-        Este projeto tem como objetivo facilitar o uso do GPT da OpenAI, para criação automática de páginas HTML, com
-        prompts compostos (incluindo geração de imagens). Além disso ele também serve como uma rede social com todas as
-        funcionalidades básicas, de curtidas, seguidores, compartilhamentos, comentários, coleções. O projeto foi feito
-        100% por mim, incluindo todas as áreas de integração, frontend, backend, design.
+        O objetivo deste projeto é otimizar o uso do GPT da OpenAI, permitindo a geração automática de páginas HTML
+        através de prompts avançados, incluindo a criação de imagens. Adicionalmente, incorpora características de uma
+        rede social, abrangendo funcionalidades essenciais como curtidas, seguidores, compartilhamentos, comentários e
+        coleções. Fui integralmente responsável por todas as etapas do projeto, desde a integração até o design,
+        passando pelo frontend e backend.
       </p>
     ),
     stack: ["NextJS", "Mysql", "Typescript", "OpenAI"],
@@ -70,10 +66,10 @@ export const projects: Project[] = [
     name: "Thauros",
     description: (
       <p>
-        Este projeto é uma automação para busca automática no preço de relógios. Foi um projeto rápido por ser mais
-        simples: o usuário agenda uma execução do robô que vai em algumas plataformas de venda de relógios e busca o
-        preço de todos os items vendidos e apresenta no dashboard. O projeto também foi feito 100% por mim. A minha
-        decisão foi criar uma fila para execução dos robôs utilizando BullMQ (Redis).
+        O projeto consiste em uma automação destinada à busca automática de preços de relógios em diversas plataformas
+        de venda. A interface permite que o usuário programe a execução do robô, que, por sua vez, recupera e exibe os
+        preços dos itens no dashboard. Toda a concepção e desenvolvimento foi realizado exclusivamente por mim. Optei
+        por implementar uma fila para a execução dos robôs utilizando BullMQ com base em Redis.
       </p>
     ),
     stack: ["NextJS", "Mysql", "Typescript", "Redis"],
@@ -82,10 +78,11 @@ export const projects: Project[] = [
     name: "Fox Analytics",
     description: (
       <p>
-        Este projeto serve para análise da quantidade de ouvintes em determinadas rádios. O meu papel foi a construção
-        de uma automação que busca os ouvintes (através do IP) em cada rádio e fica monitorando para entender novos
-        ouvintes e antigos. Com isso foi possível construir uma base com informações relevantes (incluindo localidade,
-        tempo médio, quantidade, pico) para as rádios associadas.
+        O objetivo deste projeto é analisar a quantidade de ouvintes em rádios específicas. Fui responsável pela criação
+        de uma automação que rastreia os ouvintes por meio de seus IPs em cada estação de rádio, monitorando tanto os
+        novos quanto os antigos ouvintes. A partir desta automação, estabelecemos uma base de dados contendo informações
+        cruciais, como localização, tempo médio de escuta, quantidade total de ouvintes e picos de audiência,
+        beneficiando as rádios associadas.
       </p>
     ),
     stack: ["Typescript", "Golang", "PHP"],
@@ -95,26 +92,27 @@ export const projects: Project[] = [
     description: (
       <div>
         <p>
-          É um projeto usado para gerenciar consumidores de energia fotovoltáica. Meu papel foi a contribuição na
-          construção de parte do backend e principalmente no desenvolvimento de diversas automações:
+          O projeto visa gerenciar consumidores de energia fotovoltaica. Desempenhei um papel fundamental contribuindo
+          para o desenvolvimento do backend e, mais notavelmente, na elaboração de diversas automações:
         </p>
 
         <ul>
-          <li>Leitura e interpretação de PDFs</li>
-          <li>Leitura e interpretação de XMLs</li>
-          <li>Automações para navegar na web para download de arquivos</li>
-          <li>Upload, Download e Leitura de arquivos no S3 da AWS</li>
+          <li>Interpretação e leitura de PDFs;</li>
+          <li>Interpretação e leitura de XMLs;</li>
+          <li>Automações web para baixar arquivos;</li>
+          <li>Manipulação (upload, download e leitura) de arquivos no S3 da AWS.</li>
         </ul>
 
         <p>
-          Neste projeto aproveitei para aprimorar boas práticas de programação, construi todos os serviços modulados
-          para que fosse fácil a customização do fluxo sem influenciar outras partes.
+          Durante o desenvolvimento, dediquei-me ao aperfeiçoamento de boas práticas de programação. Projetei todos os
+          serviços de maneira modular, assegurando que qualquer customização em um fluxo não impactasse outras partes do
+          sistema.
         </p>
 
         <p>
-          Com isso foi possível construção de comandos especificos para que a automação se comportasse de diferentes
-          maneiras (Salvando em arquivo ou banco de dados, Utilizar cron ou não, PDF ou XML, mudar a fonte dos arquivos
-          entre S3 ou Local...)
+          Esse design permitiu a criação de comandos específicos, possibilitando que a automação operasse de variadas
+          formas — seja salvando em arquivo ou banco de dados, usando cron jobs ou não, optando entre PDF ou XML, e
+          alternando a fonte dos arquivos entre S3 e armazenamento local.
         </p>
       </div>
     ),
@@ -125,20 +123,22 @@ export const projects: Project[] = [
     description: (
       <div>
         <p>
-          É um dos meus projetos favoritos. Fui responsável pela criação de uma feature que permite testes iniciais de
-          Transtorno do Espectro Autista
+          Este projeto ocupa um lugar especial em minha trajetória. Nele, fui incumbido da criação de uma funcionalidade
+          que possibilita testes preliminares para o Transtorno do Espectro Autista.
         </p>
 
         <p>
-          Meu papel foi desde a estruturação do banco de dados MySQL responsável para gerenciar os testes e histórico,
-          até construção de todo um frontend por onde seria feito os testes.
+          Assumi desde a concepção da estrutura do banco de dados MySQL, encarregado de administrar os testes e
+          registros associados, até o desenvolvimento completo do frontend, interface pela qual os testes seriam
+          realizados.
         </p>
 
         <p>
-          O teste consiste em diferentes etapas: Wizard, Eye Tracking Gifs, Eye Tracking Pessoas. Para o wizard utilizei
-          uma estrutura básica para o React. O desafio começa para o Eye Tracking: foi necessário integração com
-          Webgazer para receber as coordenadas do olho, edição de imagens e gifs, interpretação das coordenadas para
-          definir a pontuação de acordo com as figuras passadas.
+          O teste é segmentado em diferentes fases: um Wizard, Eye Tracking com Gifs e Eye Tracking com Pessoas. Para o
+          Wizard, adotei uma estrutura padrão para o React. Entretanto, a complexidade aumentou no segmento de Eye
+          Tracking. Foi imprescindível integrar o Webgazer para captar as coordenadas oculares, além de trabalhar com
+          edição de imagens e gifs. A partir destas coordenadas, desenvolvi uma lógica para atribuir pontuações conforme
+          as imagens apresentadas ao usuário.
         </p>
       </div>
     ),
@@ -148,12 +148,18 @@ export const projects: Project[] = [
     name: "Curso JS Fullstack",
     description: (
       <div>
-        <p>Dentro da Green Signal existe um programa de cursos para web desde aulas de empreendimento até PHP</p>
-        <p>Eu fui chamado para criar um curso ensinando Javascript avançado (ES6+), NodeJS e ReactJS</p>
         <p>
-          Para isso tive a ideia de ensinar a criação de um Chat Realtime, onde ensinei desde o necessário do JS para
-          começar com React, criação de APIs usando ExpressJS, estruturação do banco de dados usando Prisma, conexão
-          entre Frontend e Backend, Socket.io para realtime.
+          A Green Signal oferece um programa abrangente de cursos voltados para a web, cobrindo tópicos desde
+          empreendedorismo até programação em PHP.
+        </p>
+
+        <p>Fui convidado a desenvolver um curso focado em Javascript avançado (ES6+), NodeJS e ReactJS.</p>
+
+        <p>
+          Para proporcionar uma experiência prática e enriquecedora aos alunos, propus a construção de um Chat em Tempo
+          Real. Durante o curso, abordei fundamentos de Javascript essenciais para trabalhar com React, a criação de
+          APIs utilizando ExpressJS, estruturação de banco de dados com Prisma, a integração entre Frontend e Backend e
+          a implementação do Socket.io para funcionalidades em tempo real.
         </p>
       </div>
     ),
